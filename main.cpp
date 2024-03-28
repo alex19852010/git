64e1c03a0a5de232366f9dab638e6a59e0399407
Alex Bogushenko <bogushenkoas@ukr.net>
2024-03-28 20:57:32 +0300

    2commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,36 @@
 
 using namespace std;
 
+class Dog
+{
+    public:
+
+    Dog(string _name, int _age) : name(_name)
+    {
+        if(_age > 0 && _age < 30)
+        {
+           age = _age;
+        }
+
+        
+    }
+  
+   Dog() : Dog("snow", 0){}
+   Dog(string _name) : Dog(_name, 0){}
+   Dog(int _age) : Dog("snow", _age){}
+
+    private:
+    string name;
+    int age;
+
+};
+
 int main()
 {
-    
-    cout << "hello World !!!" << endl;
+    Dog p("pushok", 10);
+    Dog d("druzgok");
+    Dog n(11);
+    Dog s;
+   
     return 0;
 }
