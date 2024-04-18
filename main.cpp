85251e72f7acfc91085c8e285c53f777d68ed0b4
Alex Bogushenko <bogushenkoas@ukr.net>
2024-04-18 16:14:31 +0300

    3

--- main.cpp
+++ main.cpp
@@ -10,13 +10,11 @@ using namespace std;
 
 int main()
 {
-<<<<<<< Updated upstream
+
 
   cout << "hello barselona" << endl;
   cout << "hello milan";
-=======
-  cout << "hello world" << endl;
- 
->>>>>>> Stashed changes
+
+
   return 0;
 }
