ccb78f5d357f7dadf4a53e9ccd536d92c4db4790
Alex Bogushenko <bogushenkoas@ukr.net>
2024-04-23 10:11:11 +0300

    16 git

--- main.cpp
+++ main.cpp
@@ -11,13 +11,15 @@ using namespace std;
 int main()
 {
 
-  cout << "real" << endl;
-  cout << "hello fiorentina" << endl;
-  cout << "hello barselona" << endl;
+  cout << "lids" << endl;
   cout << "hello milan" << endl;
   cout << "hello arsenal" << endl;
   cout << "hello portu" << endl;
   cout << "hello bavaria" << endl;
+  cout << "hello atlrtico" << endl;
+  cout << "hello braga" << endl;
+   cout << "hello legia" << endl;
+  cout << "hello astana" << endl;
 
   return 0;
 }
